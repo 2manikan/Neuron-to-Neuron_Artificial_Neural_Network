8688150019a5069626b70f72ff6faa45b994919d
Meenakshi Manikandan <2mmanikan@gmail.com>
2025-08-02 11:45:08 -0700

    optimized memory by storing weights one time instead of twice

--- graph_nn.py
+++ graph_nn.py
@@ -11,6 +11,7 @@ Created on Wed Jun 25 19:47:15 2025
 
 from LinkedListQueue import LinkedListQueue
 import math
+import matplotlib.pyplot as plt 
 
 plot_this=[]
 
@@ -48,7 +49,7 @@ def forward_pass(model, inp):
     
 
 def backpropogation(model, target, adam=False):
-    learning_rate=0.01
+    learning_rate=0.0001
     
     #calculate error (MSE)
     error=0
@@ -61,10 +62,10 @@ def backpropogation(model, target, adam=False):
        count += 1
        
        #debugging
-       # if end_node == 9:
-       #    plot_this.append(model[end_node][2])
-    #    print("final_values: ",model[end_node][2])    #just prints the predicted value
-    # print("------------------------")
+       if end_node == 9:
+          plot_this.append(model[end_node][2])
+       print("final_values: ",model[end_node][2])    #just prints the predicted value
+    print("------------------------")
     
     for end_node in range(8,10):
         fringe=[end_node]  #dfs
@@ -80,7 +81,7 @@ def backpropogation(model, target, adam=False):
                 
                 
                 #update adj_node gradient
-                model[adj_node][4] = model[current][4] * model[current][0][adj_node][0]  #dE/d(final node) * d(final node)/d(adj_node) ...
+                model[adj_node][4] = model[current][4] * model[adj_node][1][current][0]  #dE/d(final node) * d(final node)/d(adj_node) ...
                 
                 
                 #for debugging
@@ -95,23 +96,23 @@ def backpropogation(model, target, adam=False):
                 
                 if adam == True:
                     #first store weight averages
-                    if model[current][0][adj_node][1] == None:
-                        model[current][0][adj_node][1] = grad
+                    if model[adj_node][1][current][1] == None:
+                        model[adj_node][1][current][1] = grad
                     else:
-                        model[current][0][adj_node][1] = alpha * model[current][0][adj_node][1] + (1-alpha) * grad
+                        model[adj_node][1][current][1] = alpha * model[adj_node][1][current][1] + (1-alpha) * grad
                     
-                    if model[current][0][adj_node][2] == None:
-                        model[current][0][adj_node][2] = grad ** 2
+                    if model[adj_node][1][current][2] == None:
+                        model[adj_node][1][current][2] = grad ** 2
                     else:
-                        model[current][0][adj_node][2] = alpha * model[current][0][adj_node][2] + (1-alpha) * grad ** 2
+                        model[adj_node][1][current][2] = alpha * model[adj_node][1][current][2] + (1-alpha) * grad ** 2
                     
                     #then take a step
-                    model[current][0][adj_node][0] -= learning_rate *  (model[current][0][adj_node][1]/math.sqrt(model[current][0][adj_node][2] + eps))
-                    model[adj_node][1][current][0] = model[current][0][adj_node][0]
+                    model[adj_node][1][current][0] -= learning_rate *  (model[adj_node][1][current][1]/math.sqrt(model[adj_node][1][current][2] + eps))
+                    
                  
                 else:
-                    model[current][0][adj_node][0] -= learning_rate * grad   # -= since we minimize. we do += for maximize
-                    model[adj_node][1][current][0] = model[current][0][adj_node][0]
+                    model[adj_node][1][current][0]  -= learning_rate * grad   # -= since we minimize. we do += for maximize
+                     
                 
                 #for debugging
                 # print("learning rate: ",learning_rate,"------------ grad: ",grad)
@@ -150,34 +151,31 @@ model={
        
        
        #network(layer 1: 5 nodes to 3 nodes)
-       0:[{},{5:[0.1, None, None], 6:[0.1, None, None], 7:[0.1, None, None]},None, 0, None],  #input_nodes, output_nodes, value, total inputs actually recieved, node's gradient
-       1:[{},{5:[0.1, None, None], 6:[0.1, None, None], 7:[0.1, None, None]}, None, 0, None],
-       2:[{},{5:[0.1, None, None], 6:[0.1, None, None], 7:[0.1, None, None]}, None, 0, None],
-       3:[{},{5:[0.1, None, None], 6:[0.1, None, None], 7:[0.1, None, None]}, None, 0, None],
-       4:[{},{5:[0.1, None, None], 6:[0.1, None, None], 7:[0.1, None, None]}, None, 0, None],
+       0:[set(),{5:[0.1, None, None], 6:[0.1, None, None], 7:[0.1, None, None]},None, 0, None],  #input_nodes, output_nodes, value, total inputs actually recieved, node's gradient
+       1:[set(),{5:[0.1, None, None], 6:[0.1, None, None], 7:[0.1, None, None]}, None, 0, None],
+       2:[set(),{5:[0.1, None, None], 6:[0.1, None, None], 7:[0.1, None, None]}, None, 0, None],
+       3:[set(),{5:[0.1, None, None], 6:[0.1, None, None], 7:[0.1, None, None]}, None, 0, None],
+       4:[set(),{5:[0.1, None, None], 6:[0.1, None, None], 7:[0.1, None, None]}, None, 0, None],
        
        #network(layer 2: 3 nodes to one node)
-       5:[{0:[0.1, None, None], 1:[0.1, None, None], 2:[0.1, None, None], 3:[0.1, None, None], 
-           4:[0.1, None, None]},{8:[0.1, None, None], 9:[0.1, None, None]}, 0, 0, None],
-       6:[{0:[0.1, None, None], 1:[0.1, None, None], 2:[0.1, None, None], 
-           3:[0.1, None, None], 4:[0.1, None, None]},{8:[0.1, None, None], 9:[0.1, None, None]}, 0, 0, None],
-       7:[{0:[0.1, None, None], 1:[0.1, None, None], 2:[0.1, None, None], 3:[0.1, None, None], 
-           4:[0.1, None, None]},{8:[0.1, None, None], 9:[0.1, None, None]}, 0, 0, None],
+       5:[set([0,1,2,3,4]),{8:[0.1, None, None], 9:[0.1, None, None]}, 0, 0, None],
+       6:[set([0,1,2,3,4]),{8:[0.1, None, None], 9:[0.1, None, None]}, 0, 0, None],
+       7:[set([0,1,2,3,4]),{8:[0.1, None, None], 9:[0.1, None, None]}, 0, 0, None],
        
        #one node
-       8:[{5:[0.1, None, None], 6:[0.1, None, None], 7:[0.1, None, None]},{}, 0, 0, None],
-       9:[{5:[0.1, None, None], 6:[0.1, None, None], 7:[0.1, None, None]},{}, 0, 0, None]
+       8:[set([5,6,7]),{}, 0, 0, None],
+       9:[set([5,6,7]),{}, 0, 0, None]
        
        }
 
 
-for i in range(1000):
+for i in range(100):
    forward_pass(model, inp_tensor)
-   backpropogation(model, [54,143], adam=True)
+   backpropogation(model, [153,234], adam=False)
 
 
-
-
+plt.plot(plot_this)
+plt.show()
 
 
 
